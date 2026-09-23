30c5565c6b6a8cba830bb121925c37c68f8d9e95
MagnusPrintz2009p <magnusprintz@gmail.com>
2026-09-23 14:49:16 +0200

    Added stuff

--- game/controllers.py
+++ game/controllers.py
@@ -3,14 +3,17 @@ from .settings import PADDLE_LIMIT, PADDLE_SPEED, AI_SPEED
 
 def move_paddle(paddle, amount):
     """Move a paddle and keep it inside the screen."""
+    new_y = paddle.ycor() + amount
+    new_y = max(-PADDLE_LIMIT, min(PADDLE_LIMIT, new_y))
+    paddle.sety(new_y)
 
 
 def move_paddle_up(paddle):
-    """"""
+    move_paddle(paddle, PADDLE_SPEED)
 
 
 def move_paddle_down(paddle):
-    """"""
+    move_paddle(paddle, -PADDLE_SPEED)
 
 
 def move_ai(paddle, ball):
